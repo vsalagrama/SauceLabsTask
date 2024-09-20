464291924d87055a69b1472780e13f37b0b2cf91
Venkatesh <venkateshsalagrama@gmail.com>
2024-09-20 14:30:00 +0530

    converted to Headless for CI execution

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    headless: false,
+    headless: true,
     screenshot:'only-on-failure'
     
   },
